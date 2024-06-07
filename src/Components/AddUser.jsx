ae3df2192935d47c191e855a6d46578383f3ffc3
riznanasar003 <richurizna10@gmail.com>
2024-06-07 20:54:00 +0530

    created

---
+++ src/Components/AddUser.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddUser = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">FULL NAME</label>
+                            <input type="text" className="form-control" />
+                
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">ALBUM ID</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">DATE OF EVENT</label>
+                            <input type="date" name="" id="" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">EMAIL ID</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">PHONE NO</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">PRICE</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <button className="btn btn-success">SUBMIT</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default AddUser
